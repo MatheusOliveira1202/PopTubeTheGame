bdc89e0a3121bb148d80daf25b24fe6b5dcb6f0b
Matheus Oliveira <matolisou@gmail.com>
2016-10-20 11:32:46 -0200

    Added Scripts
    -Added managers scripts
    -Added Floor Functions(Create and Destroy)
    -Started Floor Behavior

---
+++ Assets/Scripts/Floor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floor : MonoBehaviour {
+
+    float floorSpeed;
+
+	void Start () 
+    {
+        floorSpeed = 0.1f;
+	}
+
+    void Movement()
+    {
+        transform.position -= new Vector3(floorSpeed,0,0);
+        if(transform.position.x <= -15)
+        {
+            DestroyFloor();
+        }
+    }
+
+    void DestroyFloor()
+    {
+        Destroy(gameObject);
+    }
+	
+	void Update () 
+    {
+        Movement();
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    GameObject Floor;
+
+	void Start () 
+    {
+        Floor = Resources.Load("Floor") as GameObject;
+        FloorInstantiater();
+	}
+
+    void FloorInstantiater()
+    {
+        Instantiate(Floor, new Vector3(0,-5,10), transform.rotation);
+    }
+
+	void Update () 
+    {
+     
+	}
+}
