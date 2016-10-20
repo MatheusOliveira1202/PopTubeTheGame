e4102bdd2e0664e6549a368846104c7c77c0d9f5
Matheus Oliveira <matolisou@gmail.com>
2016-10-20 10:19:01 -0200

    Started
    -added Play Button on Menu Scene

---
+++ Assets/Scripts/ButtomClicked.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtomClicked : MonoBehaviour {
+
+
+    public void Buttomclicked(string ButtomAction)
+    {
+        if (ButtomAction == "Play")
+        {
+            Application.LoadLevel("Game");
+        }
+    }
+
+	void Update () {
+	
+	}
+}
